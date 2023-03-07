9a2c32ad86143876f5a8554a2dfced5b15adf747
Javier Dev <javier.morenodev@gmail.com>
2023-03-07 12:29:24 -0400

    Paquete http, modelo users y primer llamado a la api

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,1 +1,2 @@
 export 'package:first/models/item_dropdown.dart';
+export 'package:first/models/users.dart';
--- lib/services/http.dart
+++ lib/services/http.dart
@@ -1,9 +1,26 @@
+import 'dart:convert';
+
+import 'package:first/models/models.dart';
+
+import 'package:http/http.dart' as http;
+
 class HttpService {
   static String _url = '';
   static String _token = '';
 
   HttpService() {
     _url = 'https://gorest.co.in/public/v2/';
-    _token = '__TOKEN__';
+    _token = '18ff4dfcee2d46f164b0fc1895d53f83d828fc208e9948caef0d7007af7bae21';
+  }
+
+  Future<List<UserModel>> getAllUsers() async {
+    final resp = await http.get(
+      Uri.parse('$_url/users'),
+      headers: {'Authorization': 'Bearer $_token'},
+    );
+
+    return (jsonDecode(resp.body) as List)
+        .map((user) => UserModel.fromJson(user))
+        .toList();
   }
 }
